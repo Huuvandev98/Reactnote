1a0f983d5ab078cddaa7dcc25d549a6816e66c69
Nguyen Vandev <nguyenhuuvan71198@gmail.com>
2022-08-26 20:48:45 +0700

    code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Body from "./layout/body/body";
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Nhac nho ngay quan trong cua ban</h1>
+      <Body />
+    </div>
+  );
+}
+
+export default App;
